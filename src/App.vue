973f51766de26e5e3f49ff76045e41aebef9e509
rusakov_aleksei <rusakov_aleksei@smartworld.team>
2022-12-05 00:22:48 +0700

    [FSP-001] create start project + some ui components and filter-block

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <layout-default />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import LayoutDefault from "@/layout/LayoutDefault.vue";
+export default defineComponent({
+  components: {
+    LayoutDefault,
+  },
+});
+</script>
+<style lang="scss">
+@import "style.scss";
+</style>
